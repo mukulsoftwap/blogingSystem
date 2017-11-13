542cb8cdb2422e6744c69c0704f56f6217500506
Mukul Sharma <mukul.sharma@xotiv.com>
2017-11-13 08:24:57 +0530

    use dynamic data in controller

--- client/app/controllers/blogCtrl.js
+++ client/app/controllers/blogCtrl.js
@@ -1,5 +1,10 @@
 angular.module('blog').controller('blogCtrl', blogCtrl);
 
-function blogCtrl($scope){
-    $scope.name = "Chalna chaheye...";
+function blogCtrl($scope, $http){
+    $scope.data = [];
+    $http.get('app/data.json').then(function(res){
+    	$scope.data = res.data;
+    }, function(err){
+    	console.log(err);
+    });
 }
